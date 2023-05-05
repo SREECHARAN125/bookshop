e5f4921a3f049f1b4fce4c26ddef544ceead653f
SreeCharan Itha <190030609cse@gmail.com>
2023-05-05 09:58:38 +0000

    commit

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -6,7 +6,7 @@ module.exports = function (){
   // Register your event handlers in here, e.g....
   this.after ('READ','Books', each => {
     if (each.stock > 111) {
-      each.title += ` -- 11% discount!`
+      each.title += ' -- 11% discount!'
     }
   })
 }
